5e52300472d769e1f4410424fda3a10c9b22a8e1
gaiiiuiiia <gaiiiuiiialikeaboss@gmail.com>
2020-10-10 18:01:40 +0300

    Added createData method to BaseAdmin
    32 lesson

--- public_html/core/admin/controller/BaseAdmin.php
+++ public_html/core/admin/controller/BaseAdmin.php
@@ -17,6 +17,7 @@ abstract class BaseAdmin extends BaseController
 
     protected $table;
     protected $columns;
+    protected $data;
 
     protected $menu;
     protected $title;
@@ -51,7 +52,7 @@ abstract class BaseAdmin extends BaseController
         header("Cache-Control: post-check=0,pre-check=0");
     }
 
-    protected function exectBase(){
+    protected function execBase(){
         self::inputData();
     }
 
@@ -71,6 +72,73 @@ abstract class BaseAdmin extends BaseController
         if (!$this->columns){
             new RouteException('Не найдены поля в таблице - ' . $this->table, 2);
         }
+    }
+
+    protected function createData($arr = [], $add = true){
+
+        $fields = [];
+        $order = [];
+        $order_direction = [];
+
+        if ($add){
+
+            if (!$this->columns['id_row']){
+                return $this->data = [];
+            }
+            $fields[] = $this->columns['id_row'] . ' as id';
+
+            if ($this->columns['name']) $fields['name'] = 'name';
+            if ($this->columns['img']) $fields['img'] = 'img';
+
+            if (count($fields) < 3){
+                foreach ($this->columns as $key => $item){
+                    if (!$fields['name'] && strpos($key, 'name') !== false){
+                        $fields['name'] = $key . ' as name';
+                    }
+                    if (!$fields['img'] && strpos($key, 'img') === 0){
+                        $fields['img'] = $key . ' as img';
+                    }
+                }
+            }
+            if ($arr['fields']){
+                $fields = Settings::instance()->arrayMergeRecursive($fields, $arr['fields']);
+            }
+            if ($this->columns['parent_id']){
+                if (!in_array('parent_id', $fields)) $fields[] = 'parent_id';
+                $order[] = 'parent_id';
+            }
+            if ($this->columns['menu_position']) $order[] = 'menu_position';
+                elseif ($this->columns['date']){
+
+                    if ($order) $order_direction = ['ASC', 'DESC'];
+                        else $order_direction[] = ['DESC'];
+
+                    $order[] = 'date';
+                }
+
+            if ($arr['order']){
+                $order = Settings::instance()->arrayMergeRecursive($order, $arr['order']);
+            }
+            if ($arr['order_direction']){
+                $order_direction = Settings::instance()->arrayMergeRecursive($order_direction, $arr['order_direction']);
+            }
+        }
+        else{
+
+            if (!$arr) return $this->data = [];
+
+            $fields = $arr['fields'];
+            $order = $arr['order'];
+            $order_direction = ['order_direction'];
+        }
+
+        $this->data = $this->model->get($this->table, [
+            'fields' => $fields,
+            'order' => $order,
+            'order_direction' => $order_direction,
+        ]);
+
+        exit;
 
     }
 
--- public_html/core/admin/controller/ShowController.php
+++ public_html/core/admin/controller/ShowController.php
@@ -8,9 +8,11 @@ class ShowController extends BaseAdmin
 {
 
     protected function inputData(){
-        $this->exectBase();
+        $this->execBase();
 
         $this->createTableData();
+
+        $this->createData();
 
         exit();
     }
