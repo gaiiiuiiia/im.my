84ed0432819766a06c1fb09ca4740119f47551da
Максим <gaiiiuiiialikeaboss@gmail.com>
2020-11-30 11:32:59 +0300

    68 начал работу с js

--- public_html/core/admin/view/js/scripts.js
+++ public_html/core/admin/view/js/scripts.js
@@ -1,8 +1,9 @@
-
-Ajax({type: 'POST'})
-    .then((res) => {
-        console.log('успех - ' + res);
-    })
-    .catch((res) => {
-        console.log('ошибка - ' + res);
-    });
+document.querySelector('sitemap-button').onclick = (e) => {
+    Ajax({type: 'POST'})
+        .then((res) => {
+            console.log('успех - ' + res);
+        })
+        .catch((res) => {
+            console.log('ошибка - ' + res);
+        });
+}
--- public_html/index.php
+++ public_html/index.php
@@ -13,6 +13,8 @@ use core\base\exceptions\RouteException;
 use core\base\controller\BaseRoute;
 use core\base\exceptions\DbException;
 
+if ($_POST) exit('AJAX');
+
 try{
     BaseRoute::routeDirection();
 }
