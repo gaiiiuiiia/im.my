30c0d64f5a6d8c0f37973b8d1ee410685e07624b
sapun <safiullin.fanil@bk.ru>
2021-01-17 18:08:38 +0300

    content add

--- public_html/templates/default/index.php
+++ public_html/templates/default/index.php
@@ -1,41 +1,129 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/css/bootstrap.min.css">
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.6/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
-</head>
 <body>
-<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-    <ol class="carousel-indicators">
-        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-    </ol>
-    <div class="carousel-inner">
-        <div class="carousel-item active">
-            <img class="d-block w-100" src="..." alt="Первый слайд">
+
+
+<div class="page-content">
+
+    <section>
+        <?php include "./header.html" ?>
+    </section>
+
+    <section>
+        <div class="col-lg-12 d-flex justify-content-center">
+            <div class="col-lg-10 d-flex justify-content-center">
+                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img class="d-block w-100" src="https://brafab.su/uploads/news/1280196846_orig.jpg" alt="Первый слайд">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" src="https://blog.wildberries.ru/wp-content/uploads/2015/01/warm_sale_20_t_j15.jpg" alt="Второй слайд">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" src="http://i.mycdn.me/i?r=AzEPZsRbOZEKgBhR0XGMT1RkQeRSutDOI8ExFLRfYtrBhKaKTM5SRkZCeTgDn6uOyic" alt="Третий слайд">
+                        </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
+            </div>
         </div>
-        <div class="carousel-item">
-            <img class="d-block w-100" src="..." alt="Второй слайд">
+    </section>
+
+    <section>
+
+
+        <a id="Woman"></a>
+
+        <h2 id="blWoman">Женщинам</h2>
+        <div class="col-lg-12 d-flex justify-content-center">
+            <div class="col-lg-10 d-flex justify-content-center">
+                <div class="row justify-content-around">
+                    <div class="col-4">
+                        <div class="card rounded">
+                            <img src="https://a.lmcdn.ru/img600x866/A/D/AD002EWJMSX5_12029618_2_v1_2x.jpg" class="w-100 card-img-top" alt="pic">
+                            <div class="card-body">
+                                <h5 class="card-title">Куртки </h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="card rounded">
+                            <img src="https://a.lmcdn.ru/img600x866/M/P/MP002XW0EAQX_11783955_1_v1_2x.jpg" class="w-100 card-img-top" alt="pic">
+                            <div class="card-body">
+                                <h5 class="card-title">Платья</h5>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="card rounded">
+                            <img src="https://a.lmcdn.ru/img600x866/M/P/MP002XW03E0K_12462622_2_v1_2x.jpg" class="w-100 card-img-top" alt="pic">
+                            <div class="card-body">
+                                <h5 class="card-title">Спортивная одежда</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="carousel-item">
-            <img class="d-block w-100" src="..." alt="Третий слайд">
+
+    </section>
+
+    <section>
+        <a id="Men"></a>
+        <h2 id="blMan">Мужчинам</h2>
+        <div class="col-lg-12 d-flex justify-content-center">
+            <div class="col-lg-10 d-flex justify-content-center">
+                <div class="row justify-content-around">
+                    <div class="col-4">
+                        <div class="card rounded">
+                            <img src="https://a.lmcdn.ru/img600x866/M/P/MP002XM0YDNE_6110832_1_v1.jpg" class="w-100 card-img-top" alt="pic">
+                            <div class="card-body">
+                                <h5 class="card-title">Рубашки </h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="card rounded">
+                            <img src="https://a.lmcdn.ru/img600x866/M/P/MP002XM22ABM_12837021_1_v1_2x.jpg" class="w-100 card-img-top" alt="pic">
+                            <div class="card-body">
+                                <h5 class="card-title">Джинсы</h5>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="card rounded">
+                            <img src="https://a.lmcdn.ru/img600x866/M/P/MP002XM0MUZQ_12211487_1_v1_2x.jpg" class="w-100 card-img-top" alt="pic">
+                            <div class="card-body">
+                                <h5 class="card-title">Куртка утепленная</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
-    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-    </a>
-</div>
+
+    </section>
+
+    <section>
+        <?php include "./footer.html" ?>
+    </section>
+
+
 
 </body>
 </html>
+
